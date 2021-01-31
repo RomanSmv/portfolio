ffcc29f5b7907caacbbcb65008994c8433c07477
unknown <Razvedchik008@gmail.com>
2021-01-31 21:30:15 +0300

    first commit

---
+++ src/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Main.module.css';
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+<div className={style.mainBlock}></div>
+<div className={style.text}></div>
+        </div>
+    )
+}
+
+export default Main
